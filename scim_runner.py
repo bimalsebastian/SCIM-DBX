da35e7b14e7dcb5e68c9ff63f095907dc1cd4142
bimalsebastian <39729297+bimalsebastian@users.noreply.github.com>
2024-06-27 14:59:26 +0100

    Added Condition to check if nested groups should be extracted before syncing

--- scim_runner.py
+++ scim_runner.py
@@ -1,46 +1,81 @@
 from scim_integrator import scim_integrator
-import time 
-import requests, json
-import pandas as pd
+
 import yaml
+import json
+import os
+import time
+import requests
+import pandas as pd
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor,as_completed
+from io import BytesIO
 
-
-
+current_directory = os.getcwd()
+print("Current working directory:", current_directory)
 
 # config_path = "../Alternate Configs/config bimal.yml"
-config_path =  "config.yml"
+config_path =  '/Users/bimal.sebastian/SourceCode/BP UC Workspace ACL-Performance/configs/EAScriptPython_config.yml'
+
 
 
 
 if __name__ == '__main__':
     config = ''
+    
     with open(config_path, 'r') as file:
-      base_config = yaml.safe_load(file)
-    with open(base_config['groups_to_sync_path']) as file:
-       groups_to_sync = json.load(file)
-      
-    scim_runner = scim_integrator(base_config['config'],base_config['dbx_config'],groups_to_sync ,base_config['LOG_FILE_NAME'] ,base_config['LOG_FILE_LOCATION'],is_dryrun =  base_config['is_dryrun'], Scalable_SCIM_Enabled = False, cloud_provider ='Azure')
+        base_config = yaml.safe_load(file)
 
-    scim_runner.auth_aad(True)
+    if base_config['extract_all_nested_groups']:
+      with open(base_config['groups_to_sync_path']) as file:
+        groups_to_sync = json.load(file)
+      nested_runner = scim_integrator(base_config['config'],
+                                    base_config['dbx_config'],
+                                    groups_to_sync ,base_config['LOG_FILE_NAME'] ,
+                                    base_config['LOG_FILE_LOCATION'],
+                                    is_dryrun =  base_config['is_dryrun'], 
+                                    Scalable_SCIM_Enabled = True, 
+                                    cloud_provider ='Azure')
+
+      nested_runner.auth_aad(True) 
+      
+      nested_runner.populate_groups_to_sync(base_config['groups_to_sync_path'])
+
+
+    with open(base_config['groups_to_sync_path']) as file:
+      groups_to_sync = json.load(file)
+       
+
+    scim_runner = scim_integrator(base_config['config'],
+                                  base_config['dbx_config'],
+                                  groups_to_sync ,base_config['LOG_FILE_NAME'] ,
+                                  base_config['LOG_FILE_LOCATION'],
+                                  is_dryrun =  base_config['is_dryrun'], 
+                                  Scalable_SCIM_Enabled = True, 
+                                  cloud_provider ='Azure')
+
+    scim_runner.auth_aad(True) 
+    
     scim_runner.auth_aad(False)
-    # scim_runner.get_workspaces_list()
+
 
     if base_config['deactivate_deleted_users']:
-      begin = time.time() 
+      begin = time.time()
       scim_runner.deactivate_deleted_users()
-      end = time.time() 
+      end = time.time()
       print("Time taken to deactivate deleted users is", end-begin) 
-
 
     begin = time.time() 
     scim_runner.sync_users()
     end = time.time() 
     print("Time taken to execute the sync users is", end-begin) 
 
+    # groups_df = scim_runner.get_all_groups_aad_member_count()
+
     begin = time.time() 
     scim_runner.sync_groups()
     end = time.time() 
     print("Time taken to execute the sync groups is", end-begin) 
+    
 
     begin = time.time() 
     scim_runner.sync_mappings()
@@ -52,8 +87,3 @@ if __name__ == '__main__':
       scim_runner.deactivate_orphan_users()
       end = time.time() 
       print("Time taken to deactivate orphan users is", end-begin) 
-
-
-
- 
-    
